48ccece72622a031a49e1c19e5707559770c2b10
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-03 11:48:13 -0500

    I have added changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
-
-    }
-}
+    public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        {
+            System.out.println("Please type in your first name: ");
+            String name = scanner.nextLine().toLowerCase();
+            if (name.equals("alice") || name.equals("bob")) {
+                System.out.println("Hello " + name.substring(0, 1) + name.substring(1) + ", have a wonderful day!");
+            } else {
+                System.out.println("Get out of here!");
+            }
+        }
+    }}
